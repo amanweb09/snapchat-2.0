e7242f57be25ae919c644909c9c2a1775ce477f1
Aman <amankhanna224466@gmail.com>
2022-09-10 13:01:51 +0530

    snap opened

---
+++ client/src/components/snaps/ReceiverCard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+
+const ReceiverCard = ({ openSnap, snap }) => {
+
+    const [myRec, setMyRec] = useState({})
+
+    const { user } = useSelector((state) => state.user)
+
+    useEffect(() => {
+
+        const userObj = snap.recepients.filter((rec) => { return rec.receiver._id === user._id })
+        setMyRec(userObj[0])
+
+    }, [])
+
+    return (
+        <div
+            onClick={() => { openSnap(snap.snap, snap._id) }}
+            className='w-full px-4 py-2 flex items-center justify-evenly'>
+
+            <img
+                src='/images/bitmoji-3.png'
+                alt="bitmoji"
+                className="h-14" />
+
+            <div className="flex-1">
+                <span className={`font-bold text-lg block capitalize`}>{snap.sender.name}</span>
+                <div className="flex">
+                    {
+                        myRec.isOpened === true ?
+                            
+                            <div
+                                style={{ width: 18, height: 18 }}
+                                className="border-2 border-solid border-red-500 rounded-md"></div>
+                            :
+                        <div
+                            style={{ width: 18, height: 18 }}
+                            className="bg-red-500 rounded-md"></div>
+                    }
+                    <span className="font-bold text-red-500 text-sm ml-2">New Snap</span>
+                    <span className="text-sm text-gray-300">13h</span>
+                </div>
+            </div>
+
+            <div className='bg-black/25 p-2 rounded-md'>
+                <img src="/images/camera.png" alt="camera" className='h-6' />
+            </div>
+        </div>
+    )
+}
+
+export default ReceiverCard
---
+++ client/src/components/snaps/SenderCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const SenderCard = ({ rec }) => {
+    return (
+        <div className='w-full px-4 py-2 flex items-center justify-evenly'>
+            <img src='/images/bitmoji-3.png' alt="bitmoji" className="h-14" />
+
+            <div className="flex-1">
+                <span className={`font-bold text-lg block capitalize`}>{rec.receiver?.name}</span>
+                <div className="flex">
+                    {
+                        rec.isOpened ?
+                            <>
+                                <img
+                                    src="/images/opened.png"
+                                    style={{ width: 18, height: 18 }} />
+                                <span className="font-bold text-red-500 text-sm ml-2">Opened</span>
+                                <span className="text-sm text-gray-300">3mins ago</span>
+                            </>
+                            :
+                            <>
+                                <img
+                                    src="/images/sent.png"
+                                    style={{ width: 18, height: 18 }} />
+                                <span className="font-bold text-red-500 text-sm ml-2">Snap sent</span>
+                                <span className="text-sm text-gray-300">13h</span>
+                            </>
+                    }
+
+                </div>
+            </div>
+
+            <div className='bg-black/25 p-2 rounded-md'>
+                <img src="/images/camera.png" alt="camera" className='h-6' />
+            </div>
+        </div>
+    )
+}
+
+export default SenderCard
